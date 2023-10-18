0a9c44960b3d276d93ff1714f6e82075071ce05b
Benjamin Reiersølmoen <benjamin@contracting.works>
2023-10-18 08:20:51 +0200

    init project

--- projects/expansion/ag-grid-angular/src/public-api.ts
+++ projects/expansion/ag-grid-angular/src/public-api.ts
@@ -2,13 +2,6 @@
  * Public API Surface of ag-grid-angular
  */
 
-export * from './lib/ag-grid-expansion/ag-grid-expansion.module';
-export * from './lib/ag-grid-expansion/ag-grid-expansion.component';
-export * from './lib/ag-grid-expansion/components/ag-grid-toolbar-action/ag-grid-toolbar-action.component';
-export * from './lib/ag-grid-expansion/interfaces/ag-grid-context';
-export * from './lib/ag-grid-expansion/interfaces/ag-grid-event';
-export * from './lib/ag-grid-expansion/interfaces/ag-grid-filters';
-export * from './lib/ag-grid-expansion/interfaces/ag-grid-options';
-export * from './lib/ag-grid-expansion/interfaces/ag-grid-toolbar-action';
-
-export * from './lib/ag-grid-expansion/actions-sets';
+export * from './lib/ag-grid-angular.service';
+export * from './lib/ag-grid-angular.component';
+export * from './lib/ag-grid-angular.module';
