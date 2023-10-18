158c7ae4659ac011e2ee2ac4f54cf4a03cb3365a
Benjamin Reiersølmoen <benjamin@contracting.works>
2023-10-18 08:42:43 +0200

    add storybook

--- projects/expansion/ag-grid-angular/src/public-api.ts
+++ projects/expansion/ag-grid-angular/src/public-api.ts
@@ -1,7 +1,3 @@
 /*
  * Public API Surface of ag-grid-angular
  */
-
-export * from './lib/ag-grid-angular.service';
-export * from './lib/ag-grid-angular.component';
-export * from './lib/ag-grid-angular.module';
